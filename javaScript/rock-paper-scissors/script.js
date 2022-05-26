c44376ecfdab6614002e4c4358bc362362757946
pablo <berwigpablo@gmail.com>
2022-05-26 14:51:09 -0300

    rock-paper-scissors game functional

--- javaScript/rock-paper-scissors/script.js
+++ javaScript/rock-paper-scissors/script.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+const selectionArray = ['rock','paper','scissors'];
+const computerSelection = computerPlay();
+let playerSelection = prompt('Choose your selection between rock, paper or scissors');
+playerSelection = playerSelection.toLowerCase();
+
+function computerPlay(){
+    const randomNumber = Math.floor(Math.random() * selectionArray.length);
+    return selectionArray[randomNumber];
+}
+
+function playRound(){
+    if (computerSelection === playerSelection){
+        alert('It\'s a tie!');
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors'){
+        alert('The computer wins!');
+    } else if (computerSelection === 'paper' && playerSelection === 'rock'){
+        alert('The computer wins!');
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper'){
+        alert('The computer wins!');
+    } else {
+        alert('You win!');
+    }
+}
+
+console.log(`Computer selection: ${computerSelection}`);
+console.log(`Player selection: ${playerSelection}`);
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
